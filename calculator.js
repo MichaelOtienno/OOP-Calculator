c596aef36a7c091ba7c12b3b8206c8f3d5b8676b
MichaelOtienno <michealvenum@gmail.com>
2023-10-18 08:57:54 +0300

    added media queries

--- calculator.js
+++ calculator.js
@@ -28,7 +28,7 @@ class Calculator {
 
     this.operators.forEach(operator => {
       operator.addEventListener('click', () => {
-        this.appendToDisplay(button.textContent);
+        this.appendToDisplay(operator.textContent);
       });
     });
 
